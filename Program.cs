c2d68a86b1c04795fd8e30d7fdf6f09e7b7ac511
Gøran Smistad <goran@smistad.org>
2020-09-15 13:02:37 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrintSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
